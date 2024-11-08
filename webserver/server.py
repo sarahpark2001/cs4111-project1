9407d4c48b5b3a1b7340eafc4736b2efd6a13e0d
Sarah Park <shpark23@amherst.edu>
2024-11-08 11:52:51 -0500

    change

--- webserver/server.py
+++ webserver/server.py
@@ -51,8 +51,8 @@ engine = create_engine(DATABASEURI)
 
 
 # Here we create a test table and insert some values in it
-engine.execute("""DROP TABLE IF EXISTS ewu.test;""")
-engine.execute("""CREATE TABLE IF NOT EXISTS ewu.test (
+engine.execute("""DROP TABLE IF EXISTS zz2941.test;""")
+engine.execute("""CREATE TABLE IF NOT EXISTS zz2941.test (
   id serial,
   name text
 );""")
@@ -120,7 +120,7 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM ewu.test")
+  cursor = g.conn.execute("SELECT name FROM zz2941.test")
   names = []
   for result in cursor:
     names.append(result['name'])  # can also be accessed using result[0]
@@ -179,7 +179,7 @@ def another():
 def add():
   name = request.form['name']
   print(name)
-  cmd = 'INSERT INTO ewu.test(name) VALUES (:name1), (:name2)';
+  cmd = 'INSERT INTO zz2941.test(name) VALUES (:name1), (:name2)';
   g.conn.execute(text(cmd), name1 = name, name2 = name);
   return redirect('/')
 
