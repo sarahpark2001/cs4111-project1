11706dd8aa9910aebcd28071d983330291f504c5
friedmanholley <gf2501@columbia.edu>
2024-11-09 10:23:16 -0500

    add session import

--- webserver/server.py
+++ webserver/server.py
@@ -18,7 +18,7 @@ Read about it online.
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, session, Response
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
